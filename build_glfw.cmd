435292232b979aad06d0a9052c9962888da2d61a
Alexander <alexxdaas@gmail.com>
2022-04-10 12:12:23 +0500

    update build scripts

---
+++ build_glfw.cmd
@@ -0,0 +1,8 @@
+cd deps
+cmake -G "Visual Studio 17 2022" -A x64 -S glfw -B glfw-build -D BUILD_SHARED_LIBS=true -D GLFW_BUILD_TESTS=false -D GLFW_BUILD_DOCS=false -D GLFW_BUILD_EXAMPLES=false
+SETLOCAL
+Set currentDirectory=%CD%
+cd "C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin" 
+MSBuild.exe %currentDirectory%\glfw-build\GLFW.sln /p:Configuration=Debug /p:Platform=x64
+MSBuild.exe %currentDirectory%\glfw-build\GLFW.sln /p:Configuration=Release /p:Platform=x64
+ENDLOCAL
