d6662fe6eff698cd5e298968c056cf7db793ddeb
Alexander <alexxdaas@gmail.com>
2022-04-11 11:01:11 +0500

    add ImGui integration

--- graphics-engine/main.cpp
+++ graphics-engine/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main(char* argc, char* argv[])
 {
 
+	//Init GLFW
 	if (!glfwInit())
 		return -1;
 
+	//CreateWindow
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Diplom", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
@@ -17,14 +25,45 @@ int main(char* argc, char* argv[])
 		return -1;
 	}
 
+	//Init ImGui
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 460");
+
+
 	glClearColor(1.0f, 1.0f, 0.0f, 0.0f);
+	float color[3];
 	while(!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+		//Start New ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::ColorPicker3("color", color);
+
+		//Render ImGui frame
+		ImGui::Render();
+
+		//Submit command to GPU(need bound opengl context)
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	std::cout << "Hello, world!" << std::endl;
+	//Terminate ImGui
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	//Terminate GLFW
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
