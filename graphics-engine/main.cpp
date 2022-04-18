b187c4c03eace2e6a68afb8b6e3691e593d3d085
daas <daas@skbkontur.ru>
2022-04-18 10:12:52 +0500

    proxy commit

--- graphics-engine/main.cpp
+++ graphics-engine/main.cpp
@@ -213,7 +213,7 @@ int main(char* argc, char* argv[])
 		ImGui::NewFrame();
 
 		glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-		glClearColor(1, 0, 0, 0.0f);
+		glClearColor(color[0], color[1], color[2], 0.0f); 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
 
