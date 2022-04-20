14a2009bd526705b0228ab41640d22dfa6c3d857
daas <daas@skbkontur.ru>
2022-04-20 15:36:34 +0500

    ..

--- graphics-engine/importers/assimp_model_importer.h
+++ graphics-engine/importers/assimp_model_importer.h
@@ -9,15 +9,15 @@
 namespace dengine{
 	class AssimpModelImporter : public IModelImporter {
 	public:
-		AssimpModelImporter(std::shared_ptr<spdlog::logger> log);
+		AssimpModelImporter(const std::shared_ptr<spdlog::logger>& log);
 		Model Import(std::pmr::string path) override;
 
 	private:
-		std::pmr::vector<dengine::Texture> loadEmbededTextures(const aiScene* scene);
-		dengine::Texture loadTextureFromMemmory(unsigned char* zipData, unsigned len);
-		std::pmr::vector<dengine::Material> loadMaterials(const aiScene* scene);
+		static std::pmr::vector<dengine::Texture> loadEmbededTextures(const aiScene* scene);
+		static dengine::Texture loadTextureFromMemmory(unsigned char* zipData, unsigned len);
+		static std::pmr::vector<dengine::Material> loadMaterials(const aiScene* scene);
 		void processNode(const aiNode* node, const aiScene* scene, std::pmr::vector<dengine::Mesh>& meshes);
-		dengine::Mesh processMesh(const aiMesh* mesh, const  aiScene* scene);
+		static dengine::Mesh processMesh(const aiMesh* mesh, const  aiScene* scene);
 
 		Assimp::Importer importer;
 		std::shared_ptr<spdlog::logger> log;
